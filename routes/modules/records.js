dc06f6d9459e5ccc9a9367b63658d037f123b9c9
Benson.YBS <yehbo0921@gmail.com>
2022-12-04 00:59:53 +0800

    feat: router and page of CRUD record But record don't need detail router, delete show.hbs

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,61 +1,50 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
 const Record = require('../../models/record')
 
-//Create new record
+// Create new record page
 router.get('/new', (req, res) => {
-  res.render('new')
+  return Category.find()
+    .lean()
+    .then(categories => res.render('new', { categories }))
+    .catch(err => console.log(err))
 })
+// Create new record
 router.post('/', (req, res) => {
-  //Delete property of restaurant object with empty input value
-  for (let prop in req.body) {
-    if (req.body[prop] === '') {
-      delete req.body[prop]
-    }
-  }
   // Add user id
   req.body.userId = req.user._id
-  return Restaurant.create(req.body)
+  console.log(req.body)
+  return Record.create(req.body)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-//Show restaurant detail
-router.get('/:restaurant_id', (req, res) => {
+// Update record
+router.get('/:record_id/edit', (req, res) => {
   const userId = req.user._id
-  const _id = req.params.restaurant_id
-  Restaurant.findOne({ _id, userId }) // Change findById() to findOne() to apply multiple query condition
+  const _id = req.params.record_id
+  Record.findOne({ _id, userId })
     .lean()
-    .then(restaurant => { res.render('show', { restaurant }) })
+    .then(record => res.render('edit', { record }))
     .catch(error => console.log(error))
 })
-//Update restaurant
-router.get('/:restaurant_id/edit', (req, res) => {
+router.put('/:record_id', (req, res) => {
   const userId = req.user._id
-  const _id = req.params.restaurant_id
-  Restaurant.findOne({ _id, userId })
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-router.put('/:restaurant_id', (req, res) => {
-  const userId = req.user._id
-  const _id = req.params.restaurant_id
-  Restaurant.findOne({ _id, userId })
-    .then(restaurant => {
-      for (let prop in req.body) {
-        restaurant[prop] = req.body[prop]
-      }
-      return restaurant.save()
+  const _id = req.params.record_id
+  Record.findOne({ _id, userId })
+    .then(record => {
+      record = req.body
+      return record.save()
     })
-    .then(() => res.redirect(`/restaurants/${_id}`))
-    .catch(error => console.log(error))
+    .then(() => res.redirect(`/`))
+    .catch(err => console.log(err))
 })
-//Delete restaurant
-router.delete('/:restaurant_id', (req, res) => {
+// Delete record
+router.delete('/:record_id', (req, res) => {
   const userId = req.user._id
-  const _id = req.params.restaurant_id
-  Restaurant.findOne({ _id, userId })
-    .then(restaurant => { return restaurant.remove() })
+  const _id = req.params.record_id
+  Record.findOne({ _id, userId })
+    .then(record => { return record.remove() })
     .then(() => {
       res.redirect('/')
     })
