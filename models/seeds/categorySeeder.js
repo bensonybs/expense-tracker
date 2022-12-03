2f5abb066450bb0ea7ec17c7256d67a4acc1c13b
Benson.YBS <yehbo0921@gmail.com>
2022-12-03 21:44:17 +0800

    fix: rewrite promise to add categoryId to record properly Previous commit in attach categoryId will fail, watch the difference between two commits

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -4,20 +4,20 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const db = require('../../config/mongoose.js')
 const SEED_CATEGORIES = require('./category.json')
-const DROP_OLD_COLLECTIONS = true
+const DROP_OLD_COLLECTIONS = false //Only for develope
 db.once('open', () => {
-  async function DropOldCollections() {
-    await Category.collection.drop()
-    console.log('Drop old collections successfully.')
-  }
   if (DROP_OLD_COLLECTIONS) {
+    async function DropOldCollections() {
+      await Category.collection.drop()
+      console.log('Drop old collections successfully.')
+    }
     DropOldCollections()
   }
   Category.insertMany(SEED_CATEGORIES)
     .then(() => console.log('Seed category imported.'))
     .catch(err => console.log(err))
     .finally(() => {
-      console.log('End of category seeder process')
+      console.log('End of category seeder process\n')
       process.exit()
     })
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -9,15 +9,20 @@ const db = require('../../config/mongoose.js')
 const SEED_CATEGORIES = require('./category.json')
 const SEED_USERS = require('./user.json')
 const SEED_RECORDS = require('./record.json')
-const DROP_OLD_COLLECTIONS = false
+const DROP_OLD_COLLECTIONS = false //Only for develope
 db.once('open', () => {
   if (DROP_OLD_COLLECTIONS) {
-    Promise.all([
-      Record.collection.drop(),
-      User.collection.drop()
-    ])
-      .then('Drop old collections successfully.')
-      .catch(err => console.log(err))
+    async function DropOldCollections() {
+      try {
+        await Record.collection.drop()
+        await User.collection.drop()
+        console.log('Drop old collections successfully.')
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+    DropOldCollections()
   }
   Promise.all(SEED_USERS.map(seedUser => {
     const { name, email, password } = seedUser
@@ -26,29 +31,36 @@ db.once('open', () => {
       name,
       email,
       password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)),
-    }).then(user => {
-      // filter records of the user
-      const records = SEED_RECORDS.filter(record => record.user_id === user_id)
-      // set record.userId = user._id
-      records.map(record => record.userId = user._id)
-      
-      records.map(record => {
-        // find category._id (find category name and query database of that name)
-        const categoryName = SEED_CATEGORIES.find(category => category.id === record.category_id).name
-        Category.findOne({ name: categoryName }).then(category => {
-          // set record.categoryId = category._id and insert into database
-          record.categoryId = category._id
-          return record
-        })
-        
-      })
-      return Record.insertMany(records)
     })
+      .then(user => {
+        // filter records of the user
+        const records = SEED_RECORDS.filter(record => record.user_id === user_id)
+        // set record.userId = user._id
+        records.map(record => record.userId = user._id)
+        return records
+      })
+      .then(records => {
+        // find categoryId of each record by its category name 
+        return Promise.all(records.map(record => {
+          // find categoryname with category_id
+          const name = SEED_CATEGORIES.find(category => category.id === record.category_id).name
+          // query the database
+          return Category.findOne({ name })
+            .then(category => {
+              // set record.categoryId = category._id
+              record.categoryId = category._id
+              return record
+            })
+        }))
+      })
+      .then(records => {
+        return Record.insertMany(records)
+      })
   }))
     .then(() => console.log('Import seed user and record successfully.'))
     .catch(err => console.log(err))
     .finally(() => {
-      console.log('Process of record and user seeder end.')
+      console.log('Process of record and user seeder end.\n')
       db.close()
       process.exit()
     })
